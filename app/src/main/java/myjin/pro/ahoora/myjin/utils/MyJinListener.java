15a4643a2e9e757bc2f26a83d81bb73f42ece2f1
myjin <dariush.fathe@gmail.com>
2018-08-07 18:35:54 +0430

    hi

--- app/src/main/java/myjin/pro/ahoora/myjin/utils/MyJinListener.java
+++ app/src/main/java/myjin/pro/ahoora/myjin/utils/MyJinListener.java
@@ -9,6 +9,8 @@ import co.ronash.pushe.PusheListenerService;
 public class MyJinListener extends PusheListenerService {
     @Override
     public void onMessageReceived(final JSONObject message, JSONObject content) {
+        Log.e("Pushe", "content json content: " + content.toString());
+
         if(message != null && message.length() > 0) {
             Log.e("Pushe", "Custom json Message: " + message.toString());
 //            //    your code
