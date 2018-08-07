d84671744f0561e6fc7538df9d90443ab527eca9
myjin <dariush.fathe@gmail.com>
2018-08-07 18:54:49 +0430

    hi

--- app/src/main/java/myjin/pro/ahoora/myjin/utils/MyJinListener.java
+++ app/src/main/java/myjin/pro/ahoora/myjin/utils/MyJinListener.java
@@ -11,6 +11,17 @@ public class MyJinListener extends PusheListenerService {
     public void onMessageReceived(final JSONObject message, JSONObject content) {
         Log.e("Pushe", "content json content: " + content.toString());
 
+        /*{"types":[],
+"iconUrl":"http:\/\/api.pushe.co\/media\/notification-images\/20180807-eac83a786bd84179a07b92600647e1d4-xxh.png",
+"title":"تیتر",
+"bigContent":"متن",
+"summary":"null",
+"imageUrl":"http:\/\/api.pushe.co\/media\/notification-images\/20180807-a87a8dc4f93b4699876ef6159bee0d2b.png",
+"content":"متن",
+"ticker":"متن نوار اعلان",
+"bigTitle":"تیتر کامل"}
+*/
+
         if(message != null && message.length() > 0) {
             Log.e("Pushe", "Custom json Message: " + message.toString());
 //            //    your code
